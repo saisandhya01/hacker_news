3dc124bf4eb48bd46e2054e8f17f5e8b8434700a
Sai <saisandhya187@gmail.com>
2023-01-25 10:17:05 +0530

    Search query API integration

--- hacker_news_website/src/HomePage.js
+++ hacker_news_website/src/HomePage.js
@@ -8,12 +8,17 @@ const HomePage = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [totalPages, setTotalPages] = useState(0);
+  const [searchInput, setSearchInput] = useState("");
+  const [query, setQuery] = useState("");
   useEffect(() => {
     setIsLoading(true);
     const fetchData = async () => {
       try {
         const { data } = await axios.get(
-          "https://hn.algolia.com/api/v1/search?"
+          "https://hn.algolia.com/api/v1/search?",
+          {
+            params: { page: currentPage, query },
+          }
         );
         const { hits, nbPages } = data;
         setArticles(hits);
@@ -25,10 +30,27 @@ const HomePage = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [currentPage, query]);
+  const handlePageChange = (event) => {
+    console.log(event);
+    setCurrentPage(event.selected);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setQuery(searchInput);
+    setCurrentPage(0);
+  };
   return (
     <div className="container">
       <h1>Hacker news clone</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          placeholder="Search stories"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
       <div className="news-container">
         {isLoading ? (
           <p>Loading...</p>
@@ -39,10 +61,17 @@ const HomePage = () => {
         )}
       </div>
       <ReactPaginate
-        nextLabel=">>"
-        previousLabel="<<"
+        className="pagination"
+        nextLabel="Next"
+        previousLabel="Previous"
         breakLabel="..."
         forcePage={currentPage}
+        pageCount={totalPages}
+        renderOnZeroPageCount={null}
+        onPageChange={handlePageChange}
+        activeClassName="active-page"
+        previousClassName="previous-page"
+        nextClassName="next-page"
       />
     </div>
   );
